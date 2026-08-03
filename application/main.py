64033e06553b1945c7dbaef3c81535d11b9188df
DoraPecurlic <dora.pecurlic01@gmail.com>
2026-08-03 19:40:31 +0200

    Publish sensor readings over mqtt

--- application/main.py
+++ application/main.py
@@ -3,17 +3,39 @@ from pathlib import Path
 
 from sensor_simulation import SensorSimulator
 from csv_storage import CsvStorage
+from mqtt_publisher import MqttPublisher
 
 DATA_FILE_PATH_CSV = (Path(__file__).resolve().parent.parent /"data" /"measurements.csv")
 
+MQTT_HOST = "localhost"
+MQTT_PORT = 1883
+MQTT_TOPIC = "atmosense/device-F401RE/telemetry"
+
 def main() -> None:
-   simulation = SensorSimulator()
+   simulator = SensorSimulator()
    storage = CsvStorage(DATA_FILE_PATH_CSV)
 
-   for _ in range(5):
-       reading = simulation.read()
-       storage.save(reading)
-       time.sleep(1)
+   publisher = MqttPublisher(host = MQTT_HOST, port = MQTT_PORT , topic = MQTT_TOPIC)
+
+   publisher.connect()
+
+   try:
+       for _ in range(5):
+           reading = simulator.read()
+
+           storage.save(reading)
+           publisher.publish(reading)
+
+           print(
+                f"Reading #{reading.sequence_number} "
+                f"saved to CSV and published to MQTT"
+            )
+
+           time.sleep(1)
+           
+   finally:
+        publisher.disconnect()
+
 
 if __name__ == "__main__":
     main()
---
+++ application/mqtt_publisher.py
@@ -0,0 +1,40 @@
+import json
+from dataclasses import asdict
+
+import paho.mqtt.client as mqtt
+
+from sensor_reading import SensorReading
+
+class MqttPublisher:
+    def __init__(self, host:str, port: int, topic: str)->None:
+        self._host = host
+        self._port = port
+        self._topic = topic
+
+        self._client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2, client_id="atmosense-edge-gateway", protocol=mqtt.MQTTv5)
+    
+    def connect(self) -> None:
+        self._client.connect(host = self._host, port = self._port, keepalive = 60)
+
+        self._client.loop_start() #odrzava mrezu za kontinuirwanu komunikaciju
+    
+    def publish(self, data: SensorReading) -> None:
+        payload_data = asdict(data)
+
+        payload_data["recieved_at"] = data.recieved_at.strftime("%Y-%m-%d %H:%M:%S")
+
+        payload = json.dumps(payload_data)
+
+        message = self._client.publish(topic = self._topic, payload=payload, qos = 1)
+
+        if message.rc != mqtt.MQTT_ERR_SUCCESS:
+            raise ConnectionError(f"MQTT publish failed: {mqtt.error_string(message.rc)}")
+
+        message.wait_for_publish(timeout=5.0)
+
+        if not message.is_published():
+            raise TimeoutError("MQTT message was not published in time")
+    
+    def disconnect(self)->None:
+        self._client.disconnect()
+        self._client.loop_stop()
